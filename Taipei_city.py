e2c4299a299d160f8529c35ec34cb2cfcb016a00
Ohpaipai <zx40904@gmail.com>
2020-12-13 21:08:25 +0800

    data partition

--- Taipei_city.py
+++ Taipei_city.py
@@ -3,7 +3,13 @@ import numpy as np
 
 Data = pd.read_csv("./taipei_city_real_estate_transaction_v2.csv")
 
-Data = Data[~Data['urban_land_use'].isin(['Other','Others'])]
+Data1 = Data[Data['urban_land_use'].isin(['work'])]
+Data2 = Data[Data['urban_land_use'].isin(['quotient'])]
+Data3 = Data[Data['urban_land_use'].isin(['address'])]
+Data4 = Data[Data['urban_land_use'].isin(['Agriculture'])]
 
 # 產生 CSV
-Data.to_csv('New_Data.csv',encoding='utf8') 
+Data1.to_csv('work.csv',encoding='utf8') 
+Data2.to_csv('quotient.csv',encoding='utf8') 
+Data3.to_csv('address.csv',encoding='utf8') 
+Data4.to_csv('agriculture.csv',encoding='utf8') 
