21943312aee146d94663f3064a0ae686d1380913
Ohpaipai <zx40904@gmail.com>
2020-12-14 15:05:27 +0800

    dsakdasld

--- LIU.py
+++ LIU.py
@@ -1,14 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[32]:
-
-
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 import numpy as np
 
 Data = pd.read_csv("./taipei_city_real_estate_transaction_v2.csv")
+
 '''
 Data1 = Data[Data['urban_land_use'].isin(['work'])]
 Data2 = Data[Data['urban_land_use'].isin(['quotient'])]
@@ -25,31 +20,8 @@ Data4.to_csv('agriculture.csv',encoding='utf8')
 temp = pd.get_dummies(Data.district)
 temp = Data.join(temp)
 temp = temp.drop(['district'], axis=1)
-temp
+temp.to_csv('temp.csv',encoding='utf8')
 
-
-# In[5]:
-
-
-Data1
-
-
-# In[6]:
-
-
-import pandas as pd
-from sklearn.preprocessing import LabelEncoder
-Data = pd.read_csv("./work.csv")
-labelencoder = LabelEncoder()
-data_le=pd.DataFrame(Data)
-data_le['main_building_material'] = labelencoder.fit_transform(data_le['main_building_material'])
-data_dum = pd.get_dummies(data_le)
-pd.DataFrame(data_dum)
-data_dum.to_csv('work.csv',encoding='utf8')
-data_dum
-
-
-# In[ ]:
 
 
 
--- Taipei_city.py
+++ Taipei_city.py
@@ -1,9 +1,28 @@
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 import numpy as np
 
 Data = pd.read_csv("./taipei_city_real_estate_transaction_v2.csv")
 
+labelencoder = LabelEncoder()
+Data=pd.DataFrame(Data)
+#Data['main_building_material','transaction_type','main_use','carpark_category'] = labelencoder.fit_transform(Data['main_building_material','transaction_type','main_use','carpark_category'])
+Data['main_building_material'] = labelencoder.fit_transform(Data['main_building_material'])
+Data['transaction_type'] = labelencoder.fit_transform(Data['transaction_type'])
+Data['main_use'] = labelencoder.fit_transform(Data['main_use'])
+Data['carpark_category'] = labelencoder.fit_transform(Data['carpark_category'])
+
+onehotencoder = OneHotEncoder(sparse = False)
+#data_str_ohe=onehotencoder.fit_transform(Data).toarray()
+data_str_ohe=Data.reshape(-1,1)
+pd.DataFrame(data_str_ohe)
+
+#Data = pd.get_dummies(Data)
+#pd.DataFrame(Data)
+Data.to_csv('New.csv',encoding='utf8') 
+
+'''
 Data1 = Data[Data['urban_land_use'].isin(['work'])]
 Data2 = Data[Data['urban_land_use'].isin(['quotient'])]
 Data3 = Data[Data['urban_land_use'].isin(['address'])]
@@ -14,3 +33,4 @@ Data1.to_csv('work.csv',encoding='utf8')
 Data2.to_csv('quotient.csv',encoding='utf8') 
 Data3.to_csv('address.csv',encoding='utf8') 
 Data4.to_csv('agriculture.csv',encoding='utf8') 
+'''
