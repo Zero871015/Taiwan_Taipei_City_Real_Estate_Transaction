3d776195353a75188a9117f74e536f4b496229be
Ohpaipai <zx40904@gmail.com>
2020-12-28 16:36:32 +0800

    use district different data

--- LIU.py
+++ LIU.py
@@ -4,29 +4,31 @@ from sklearn.preprocessing import OneHotEncoder
 #import numpy as np
 
 Data = pd.read_csv("./taipei_city_real_estate_transaction_v2.csv")
-temp = pd.get_dummies(Data.district)
-#temp = temp.drop(['district'], axis=1)
+
+temp = pd.get_dummies(Data.urban_land_use)
 #temp = pd.concat([Data,temp])
-Data = Data.join(temp)
+Data = Data.merge(temp, left_index=True, right_index=True) 
+#Data = Data.join(temp)
 
 temp = pd.get_dummies(Data.carpark_category)
-Data = Data.join(temp)
+#Data = Data.join(temp)
+Data = Data.merge(temp, left_index=True, right_index=True) 
 
 temp = pd.get_dummies(Data.main_building_material)
 #Data = pd.merge([Data,temp])
 Data = Data.merge(temp, left_index=True, right_index=True) 
 
-temp = pd.get_dummies(Data.main_use)
+#temp = pd.get_dummies(Data.main_use)
 #Data = Data.join(temp)
 #Data = pd.concat([Data,temp])
-Data = Data.merge(temp, left_index=True, right_index=True) 
+#Data = Data.merge(temp, left_index=True, right_index=True) 
 
 temp = pd.get_dummies(Data.transaction_type)
 #temp = Data.join(temp)
 #temp = pd.concat([Data,temp])
 Data = Data.merge(temp, left_index=True, right_index=True) 
 
-Data = Data.drop(['district','transaction_type','main_use','main_building_material','carpark_category'],axis=1)
+Data = Data.drop(['urban_land_use','transaction_type','main_use','main_building_material','carpark_category','number_of_land','unit_ntd','building_age'],axis=1)
 
 #temp = temp.drop(['district'], axis=1)
 
